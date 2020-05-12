6a00da906b4791fe652b9e0313099eef460e96e3
ashakumari <ashakumari@gmail.com>
2020-05-12 23:51:24 +0530

    Introduced to the concept of functions, return values, named and multiple return values

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module example.com/user/hello
 
 go 1.14
 
-require github.com/google/go-cmp v0.4.0
+require (
+	github.com/google/go-cmp v0.4.0
+	golang.org/x/tour v0.0.0-20200508155540-0608babe047d // indirect
+)
--- hello.go
+++ hello.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math"
+
 	"example.com/user/hello/morestrings"
 	"github.com/google/go-cmp/cmp"
 )
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
 
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Now for fun read this...")
 	fmt.Println(morestrings.ReverseRunes("Hello Go!"))
+	a, b := swap("Hello", "World")
+	fmt.Println(a, b)
 	fmt.Println("Let's look at the diffs now....")
 	fmt.Println(cmp.Diff("Hello World!", "Hello Go!"))
 	fmt.Println(cmp.Diff("Hello Go!", "!oG olleH"))
+	fmt.Println("Let's have some Math fun now... ")
+	fmt.Println("Value of Pi : ", math.Pi)
+	fmt.Printf("Sum of %v, %v is %v\n", 121, 98, add(121, 98))
 }
