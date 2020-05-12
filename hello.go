6fdcdbd7f00d771cb41d484d449c99557a99ad8f
ashakumari <ashakumari@gmail.com>
2020-05-12 23:21:46 +0530

    Concept of importing packages within and outside modules as well as writing tests

--- hello.go
+++ hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	fmt.Println("Now for fun read this...")
+	fmt.Println(morestrings.ReverseRunes("Hello Go!"))
+	fmt.Println("Let's look at the diffs now....")
+	fmt.Println(cmp.Diff("Hello World!", "Hello Go!"))
+	fmt.Println(cmp.Diff("Hello Go!", "!oG olleH"))
 }
